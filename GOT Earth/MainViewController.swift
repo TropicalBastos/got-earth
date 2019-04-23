1a3c4f475bde2cab38d1cd28bba87002c520bc5d
Ian Bastos <ian-bastos@live.com>
2019-04-23 01:34:16 +0100

    braavos added

--- GOT Earth/MainViewController.swift
+++ GOT Earth/MainViewController.swift
@@ -72,7 +72,7 @@ class MainViewController: UIViewController {
         scnView.allowsCameraControl = false
         
         // show statistics such as fps and timing information
-        scnView.showsStatistics = true
+        scnView.showsStatistics = false
         
         // configure the view
         scnView.backgroundColor = UIColor.black
