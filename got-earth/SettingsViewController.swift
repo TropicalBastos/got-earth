8e1ea6215447f4facc0bf7ee43527ed4d07a712b
Ian Bastos <ian-bastos@live.com>
2019-05-10 03:48:10 +0100

    show labels controls as table cells

--- got-earth/SettingsViewController.swift
+++ got-earth/SettingsViewController.swift
@@ -8,12 +8,26 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UITableViewDataSource {
     
+    
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var closeButton: UIBarButtonItem!
     @IBOutlet weak var navigationBar: UINavigationBar!
     var primaryFont: UIFont!
     var secondaryFont: UIFont!
+    
+    struct LabelStruct {
+        var title : String
+        var checked : Bool
+        var disabled: Bool
+    }
+    
+    var labels: [LabelStruct] = [
+        LabelStruct(title: "Show Westeros", checked: true, disabled: false),
+        LabelStruct(title: "Show Essos", checked: false, disabled: true),
+        LabelStruct(title: "Show Sothoryos", checked: false, disabled: true)
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +40,8 @@ class SettingsViewController: UIViewController {
              NSAttributedStringKey.font: primaryFont!]
         
         closeButton.action = #selector(closeSettings)
+        
+        tableViewSetup()
     }
 
     @objc
@@ -38,15 +54,22 @@ class SettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableViewSetup() {
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "ShowLabelsTableViewCell", bundle: Bundle.main), forCellReuseIdentifier: "ShowLabelsTableViewCell")
+        
+        // TODO load prefs and ovveride self.labels data
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.labels.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "ShowLabelsTableViewCell", for: indexPath) as! ShowLabelsTableViewCell
+        
+        cell.label.text = self.labels[indexPath.row].title
+        return cell
+    }
 
 }
